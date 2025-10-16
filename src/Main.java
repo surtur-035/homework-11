a0fdab32ac535ca1937e1dfc37e890a7244131ea
surtur-035 <makarik2004981@gmail.com>
2025-10-16 22:18:03 +0300

    Поправил условие в задаче 2

--- src/Main.java
+++ src/Main.java
@@ -25,11 +25,11 @@ public class Main {
 
     public static void checkDeviceDate(int clientOs, int clientDeviceYear) {
 
-        if (clientOs == 0 && clientDeviceYear == 2015) {
+        if (clientOs == 0 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         } else if (clientOs == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (clientOs == 1 && clientDeviceYear == 2015) {
+        } else if (clientOs == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
         } else if (clientOs == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
